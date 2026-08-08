081649b8b30fc3f80d85c28d3386447092546d67
sonu kumar <gautamrocky909621@gmail.com>
2026-08-08 23:20:55 +0530

    nothing new to commit did for same purpose to integrate in pipeline analysis for newly added strategy

--- src/main/java/com/rag/documentprocessingpipeline/parsing/model/ParsedMetadata.java
+++ src/main/java/com/rag/documentprocessingpipeline/parsing/model/ParsedMetadata.java
@@ -3,9 +3,11 @@ package com.rag.documentprocessingpipeline.parsing.model;
 import java.util.*;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @Builder
+@ToString
 public class ParsedMetadata {
 
     @Builder.Default
